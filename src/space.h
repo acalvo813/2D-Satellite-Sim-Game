0aa775117e14f02c79ff28631c5d81d7a5de94e5
= <=>
2020-03-16 20:41:12 +0000

    multi satellite stabilize

--- src/space.h
+++ src/space.h
@@ -24,22 +24,53 @@ class SpaceController : public Process, public AgentInterface {
         //omni_apply_force(-(x()*x())*G,-(y()*y())*G);
         //apply_force(0,cos(x())*100);
        // move_toward(0, 0, 10, 100);
-        return -pos*abs(pos)*G;
+       //std::cout<<(0-G*10000)/(pos*abs(pos));
+
+        double g;
+        g=-G*pos*abs(pos);
+        // g= (0-G)/(pos*abs(pos));
+        // if ( abs(g)>2500 ){
+        //     std::cout<<"went in"<<"\n";
+        //     g=-G*pos*0.0000001;
+        //     //std::cout<< g <<"\n for robot "<< id()<< "\n";
+        // }
+        
+
+        //std::cout<<g<<"\n";
+        return g;//(0-G*10000)/(pos*abs(pos));
     }
 
 
+    void addinputkeys(){
+        if ( RIGHT ) {
+            vx = VEL_X;
+            
+        } if ( LEFT ) {
+            vx = -VEL_X;
+        } else if ( !RIGHT && !LEFT ) {
+            vx = 0;
+        }
 
-    void init() {
-        allow_rotation();
-        STABILIZING=false;
+        if ( UP ) {
+            vy = -VEL_Y;
+        }else if ( DOWN ) {
+           vy = VEL_Y;
+        } else if ( !UP && !DOWN ) {
+            vy = 0;
+        }else{
+        }
+
+    }
+    void initinputkeys(){
         watch("keydown", [&](Event& e) {
             std::string k = e.value()["key"];
             if ( k == "w" ) {
                 UP = true;
             }else if ( k == "s" ) {
-                DOWN = true;
+                DOWN = true;      
             } else if ( k == "a" ) {
                 LEFT = true;
+                
             } else if ( k == "d" ) {
                 RIGHT = true;
             } 
@@ -55,13 +86,21 @@ class SpaceController : public Process, public AgentInterface {
             } else if ( k == "d" ) {
                 RIGHT = false;
             }
-        });      
+        });  
+    }
+
+    void init() {
+        allow_rotation();
+        STABILIZING=false;
+        STABLE=true;
+        initinputkeys();
     }
     void start() {}
 
     void update() {
         double fx=0;
         double fy=0;
+        double m=0;
         
         double actualfx;
         double actualfy;
@@ -70,42 +109,27 @@ class SpaceController : public Process, public AgentInterface {
             //std::cout<<angle();
         }); 
         //apply_gravity();
-       
-        
-        if ( RIGHT ) {
-            vx = VEL_X;
-        } if ( LEFT ) {
-            vx = -VEL_X;
-        } else if ( !RIGHT && !LEFT ) {
-            vx = 0;
-        }
-
-        if ( UP ) {
-            vy = -VEL_Y;
-        }else if ( DOWN ) {
-           vy = VEL_Y;
-        } else if ( !UP && !DOWN ) {
-            vy = 0;
-        }else{
-
-        }
+       addinputkeys();
             
             
             fx = -K_X*(0-vx);
             fy = -K_X*(0-vy);
+            
             actualfx=fx*cos(angle())-fy*sin(angle());
             actualfy=fx*sin(angle())+fy*cos(angle());
         //omni_apply_force(g(x()),g(y()));    
 
         //when keys randomly stop working.... uncomment below to debug
-        // label("actual fx "+ to_string(actualfx)+ " actual fy "+to_string(actualfy),0,20);
+        //label("actual fx "+ to_string(actualfx)+ " actual fy "+to_string(actualfy),0,20);
+        //label("x "+ to_string(x())+ " y "+to_string(y()),0,20);
+        label("Distance to earth "+ to_string(height()),0,20);
         //omni_apply_force(actualfx,actualfy);
 
         omni_apply_force(actualfx+g(x()),actualfy+g(y()));
 
-        //label("real angle "+ to_string(realangle())+" built in angle() "+to_string(angle()),0,20);
-        //label("ag-atan is "+ to_string(realangle()-angleoforigin())+ " pointgatorigin is "+to_string(pointingatorigin()),0,15);
-        label("angular velocity "+to_string(angular_velocity())+ " pointgatorigin is "+to_string(pointingatorigin()),20,0);
+        //label("\nreal angle "+ to_string(realangle())+" built in angle to origin "+to_string(angleoforigin()),0,20);
+        //label("ag-atan is "+ to_string(realangle()-angleoforigin())+ " pointgatorigin is "+to_string(pointingatorigin()),0,-20);
+        //label("angular velocity "+to_string(angular_velocity())+ " pointgatorigin is "+to_string(pointingatorigin()),20,0);
 
         if (!pointingatorigin()){
                 //double fr=VEL_R*atan2(x(),-y());
@@ -115,9 +139,14 @@ class SpaceController : public Process, public AgentInterface {
                 //fr=-K_X*(angular_velocity()+0.001);
             
                 // need to apply for based off its current angular velocity = -K_X*(0-vy);
-                if (x()!=0){
-                 apply_force(0,fr*100);
-                }
+                
+                apply_force(0,fr*100);
+                
+                // if (x()>0){
+                //     apply_force(0,fr*100);
+                // }else{
+                //     apply_force(0,-fr*100);
+                // }
             //label("angular velocity "+to_string(angular_velocity())+ "is fr "+to_string(fr),20,0);
 
             // if (abs(angular_velocity())>mag){ 
@@ -131,26 +160,55 @@ class SpaceController : public Process, public AgentInterface {
             // }else{
             //     apply_force(0,-VEL_R*(realangle()-atan2(x(),-y())));
             // }
+                
             
         }
         else{
             //prevent_rotation();
-                if (abs(angular_velocity())>0.01 && !STABILIZING){ 
+                if (abs(angular_velocity())>0.01 ){
+                
                 STABILIZING=true;
+               // clear_label();
+                //label("trying to stabilize ln 157",0,0);
                 if (vr>0){
+
                     apply_force(0,-angular_velocity()*vr);
-                    vr=vr/10;
+                    
+                    vr=-vr/10;
                 }else{
                     STABILIZING=false;
-                    vr=10000000;
+                    vr=10000;
                 }
                 
-
             }
 
         }
-        
+
+           //stabilize attempt     
+        if (angular_velocity()>0.05){
+            // sta has gone out of control
       
+        //     STABLE=false;
+        //     prevent_rotation();
+        //     clear_label();
+        //     label("trying to stabilize ln 178",0,0);
+        //         if (vr>0.1){
+        //             apply_force(0,-angular_velocity()*vr*100000000000000);
+        //             vr=vr/10;
+        //         }else{
+        //             vr=100000000;
+        //             count++;
+        //             if(angular_velocity()>0.001 && count>3 ){
+        //                 allow_rotation();
+        //                 count=0;
+        //                 STABLE=true;
+        //             }
+        //         }
+        
+         }
+
+
+
         //apply_force(100,0);
 
         // if (x()!=0){
@@ -173,15 +231,16 @@ class SpaceController : public Process, public AgentInterface {
    
     double vx;
     double vy;
-    double vr=1000;
-    
-    const double G = 0.098;
+    double vr=1000000;
+    double oldav=0;
+    int count=0;
+    const double G = 0.098;//9.8*1000000;
     const double VEL_X = 20;
     const double VEL_Y = 20;
     const double VEL_R = 2000;
-    const double K_X = 1500;
-    bool LEFT, RIGHT, UP,DOWN;
-    bool STABILIZING;
+    const double K_X = 1000;
+    bool LEFT, RIGHT, UP,DOWN,STABLE,STABILIZING;
+
 
     private:
     bool pointingatorigin(){
@@ -193,7 +252,14 @@ class SpaceController : public Process, public AgentInterface {
         return remainder(angle(),2*3.14);
     }
     double angleoforigin(){
+        
        double a= atan2(x(),-y());
+       a=remainder(a,2*3.14);
+      if (abs(realangle()-a)>2){ // if at arc tan - pi pi point
+        a= realangle()+(0.01*realangle()); // if neg want more neg if pos want more pos
+        //a=3.14+3.14-(realangle()-a);
+      }
+       
        return a;
     }
 
